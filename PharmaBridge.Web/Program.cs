c57dcc1ca299bb9dd1d915c2aa5f770749febd77
Mohammed Adel <121mohamedadel121@gmail.com>
2026-03-11 22:35:56 +0200

    add test comment

--- PharmaBridge.Web/Program.cs
+++ PharmaBridge.Web/Program.cs
@@ -24,7 +24,7 @@ namespace PharmaBridge.Web
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-            //Test
+            //Test2
 
             app.MapControllers();
 
