01e90651eec8f65a36a4262b5c45ef78ad7aef83
mohamedes324 <mohamedesaam363@gmail.com>
2026-03-13 19:46:45 +0200

    Create PatientProfile and PatientAddress models

---
+++ Core/PharmaBridge.Domain/Models/User/PatientAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Domain.Models.User
+{
+    public class PatientAddress : BaseEntity<string>
+    {
+        public string AddressLine { get; set; }
+
+        public string City { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public bool IsDefault { get; set; }
+    }
+    
+}
