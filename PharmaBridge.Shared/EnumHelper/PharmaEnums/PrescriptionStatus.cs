1b772a8181eb5724a5de05b88bc999b686c4efe9
Nour Sameh <nono5noor4@gmail.com>
2026-03-13 14:34:08 +0200

    Add Pharmacy model with enums

---
+++ PharmaBridge.Shared/EnumHelper/PharmaEnums/PrescriptionStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Shared.EnumHelper.PharmaEnums
+{
+    public enum PrescriptionStatus
+    {
+        InProgress = 1,
+        Completed,
+        Cancelled,
+        Expired,
+        OutForDelivery
+    }
+}
