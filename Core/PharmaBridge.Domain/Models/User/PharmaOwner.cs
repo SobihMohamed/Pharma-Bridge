67e62ae426bf32343765007083a146af8786391d
mohamedes324 <mohamedesaam363@gmail.com>
2026-03-13 19:31:53 +0200

    Create ApplicationUser and PharmaOwner models

---
+++ Core/PharmaBridge.Domain/Models/User/PharmaOwner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Domain.Models.User
+{
+    public class PharmaOwner : BaseEntity<string>
+    {
+        public string FullName { get; set; }
+
+        public string NationalId { get; set; }
+
+        public string NationalIdFront { get; set; }
+
+        public string NationalIdBack { get; set; }
+
+        public string SyndicateCardImage { get; set; }
+
+        public bool IsVerified { get; set; }
+    }
+}
