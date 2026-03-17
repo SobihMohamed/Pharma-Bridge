27d54ee9ae9a1dbb90cd7463f7bcad3fe981be20
Nour Sameh <nono5noor4@gmail.com>
2026-03-17 22:11:50 +0200

    Configurations/bidding system config (#10)
    * Add Bid and BidItem Configurations
    
    * Add PharmaPerformSnapshotConfiguration
    
    * Remove RespondedAt property from BidConfiguration
    
    * Modify changes
    
    ---------
    
    Co-authored-by: Sobih Mohamed <158785931+SobihMohamed@users.noreply.github.com>

---
+++ Infrastructure/PharmaBridge.Persistence/Configurations/Pharma_Operations/BidConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaBridge.Domain.Models.Pharma_Requests;
+using PharmaBridge.Shared.EnumHelper.PharmaEnums;
+
+namespace PharmaBridge.Persistence.Configurations.Pharma_Operations
+{
+    public class BidConfiguration : IEntityTypeConfiguration<Bid>
+    {
+        public void Configure(EntityTypeBuilder<Bid> builder)
+        {
+            builder.ToTable("Bids");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Subtotal)
+                   .HasPrecision(18, 2)
+                   .IsRequired();
+
+            builder.Property(x => x.DiscountAmount)
+                   .HasPrecision(18, 2)
+                   .IsRequired();
+
+            builder.Property(x => x.DeliveryFee)
+                   .HasPrecision(18, 2)
+                   .IsRequired();
+
+            builder.Property(x => x.PlatformFee)
+                   .HasPrecision(18, 2)
+                   .IsRequired();
+
+            builder.Property(x => x.TotalPrice)
+                   .HasPrecision(18, 2)
+                   .IsRequired();
+
+            builder.Property(x => x.Status)
+               .HasConversion<string>()
+               .HasMaxLength(50);
+
+            builder.Property(x => x.Notes)
+                   .HasMaxLength(500);
+
+            builder.Property(x => x.SubmittedAt)
+                   .IsRequired();
+
+            builder.Property(x => x.DeliveryTimeInMinutes)
+                   .IsRequired();
+
+            // Relations 
+
+            // 12 - Bid (Many) To (1) Pharmacy 
+
+            builder.HasOne(x => x.Pharmacy)
+                   .WithMany(p => p.Bids)
+                   .HasForeignKey(x => x.PharmacyId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            // 20 - PrescriptionRequest (1) To (Many) Bid (Has)
+
+            builder.HasOne(x => x.PrescriptionRequest)
+                   .WithMany(pr => pr.Bids)
+                   .HasForeignKey(x => x.PrescriptionRequestId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+
+        }
+    }
+}
---
+++ Infrastructure/PharmaBridge.Persistence/Configurations/Pharma_Operations/BidItemConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaBridge.Domain.Models.Pharma_Requests;
+
+namespace PharmaBridge.Persistence.Configurations.Pharma_Operations
+{
+    public class BidItemConfiguration : IEntityTypeConfiguration<BidItem>
+    {
+        public void Configure(EntityTypeBuilder<BidItem> builder)
+        {
+            builder.ToTable("BidItems");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.ItemName)
+                   .HasMaxLength(100)
+                   .IsRequired();
+
+            builder.Property(x => x.UnitPrice)
+                   .HasPrecision(18, 2)
+                   .IsRequired();
+
+            builder.Property(x => x.Quantity)
+                   .IsRequired();
+
+            builder.Property(x => x.IsAlternative)
+                   .IsRequired();
+
+            builder.Property(x => x.AlternativeNote)
+                   .HasMaxLength(500);
+
+            builder.Property(x => x.LineTotal)
+                   .HasPrecision(18, 2)
+                   .IsRequired();
+
+            // Relations 
+
+            // 11 - Bid (1) To (Many) BidItems
+            // child table is useless without the parent table
+
+            builder.HasOne(x => x.Bid)
+                   .WithMany(b => b.BidItems)
+                   .HasForeignKey(x => x.BidId)
+                   .OnDelete(DeleteBehavior.Cascade);
+            
+        }
+    }
+}
