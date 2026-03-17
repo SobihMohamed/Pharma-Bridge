35e6872830381a75ea03262d403fd26d88294eb1
Nour Sameh <nono5noor4@gmail.com>
2026-03-17 20:57:11 +0200

    Add PharmaPerformSnapshotConfiguration

---
+++ Infrastructure/PharmaBridge.Persistence/Configurations/PharmaPerformSnapshotConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaBridge.Domain.Models.Pharma_Requests;
+using PharmaBridge.Shared.EnumHelper.PharmaEnums;
+
+
+namespace PharmaBridge.Persistence.Configurations
+{
+    public class PharmaPerformSnapshotConfiguration : IEntityTypeConfiguration<PharmaPerformSnapshot>
+    {
+        public void Configure(EntityTypeBuilder<PharmaPerformSnapshot> builder)
+        {
+            builder.ToTable("PharmaPerformSnapshots");
+
+            builder.HasKey(x=>x.Id);
+
+            builder.Property(x=>x.PeriodStart)
+                .IsRequired();
+
+            builder.Property(x => x.PeriodEnd)
+                .IsRequired();
+
+            builder.Property(x => x.PeriodType)
+                   .HasMaxLength(50)
+                   .IsRequired()
+                   .HasConversion(
+                       v => v.ToString(),
+                       v => (SnapshotPeriodType)Enum.Parse(typeof(SnapshotPeriodType), v)
+                   );
+
+
+            builder.Property(x => x.TotalBids)
+                .IsRequired();
+
+            builder.Property(x => x.WonOrders)
+                .IsRequired();
+
+            builder.Property(x => x.CompletedOrders)
+                   .IsRequired();
+
+            builder.Property(x => x.CancelledOrders)
+                   .IsRequired();
+
+            builder.Property(x => x.CompletionRate)
+                   .HasPrecision(5, 2)
+                   .IsRequired();
+
+            builder.Property(x => x.TotalRevenue)
+                   .HasPrecision(18, 2)
+                   .IsRequired();
+
+            builder.Property(x => x.TotalPlatformFee)
+                   .HasPrecision(18, 2)
+                   .IsRequired();
+
+            builder.Property(x => x.ComputedAt)
+                   .IsRequired();
+
+            // Relations
+
+            // PharmaPerformSnapshot (Many)  Pharmacy (1) 
+
+            builder.HasOne(x => x.Pharmacy)
+                   .WithMany(p => p.PharmaPerformSnapshots)
+                   .HasForeignKey(x => x.PharmacyId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
