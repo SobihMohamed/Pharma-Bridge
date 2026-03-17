a51f6f1c06440d8ce277f4ff214fa4add4dd236d
Mohammed Adel <121mohamedadel121@gmail.com>
2026-03-17 18:25:24 +0200

    1-add ComplaintConfiguration Class have 3 Relations (Complaint -> Order), (Complaint -> SubmittedBy) , (Complaint -> ResolvedBy) 2-add PaymentConfiguration Class have 1 Relations Payment -> Order 3-add OrderConfiguration Class have 5 Relations (Order -> PatientAddress), (Order -> Bid) , (Order -> PatientProfile) , (Order -> Pharmacy) , ( Order -> PrescriptionRequest)

---
+++ Infrastructure/PharmaBridge.Persistence/Configurations/OrdersOperationsConfig/ComplaintConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaBridge.Domain.Models.UserAccess;
+using PharmaBridge.Shared.EnumHelper.UserAccessEnums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Persistence.Configurations.OrdersOperationsConfig
+{
+    public class ComplaintConfiguration : IEntityTypeConfiguration<Complaint>
+    {
+        public void Configure(EntityTypeBuilder<Complaint> builder)
+        {
+            //تم تعبئة الكرش بنجاح 
+            builder.ToTable("Complaints");
+
+            builder.Property(c => c.Title).IsRequired().HasMaxLength(100);
+            builder.Property(c => c.Description).IsRequired().HasMaxLength(500);
+            builder.Property(c => c.AdminNotes).HasMaxLength(500);
+
+            builder.Property(c => c.Status)
+                   .HasConversion(
+                       v => v.ToString(),
+                       v => (ComplaintStatus)Enum.Parse(typeof(ComplaintStatus), v)
+                   );
+
+            //  Relationships
+
+            // Complaint -> Order
+            builder.HasOne(c => c.Order)
+                   .WithMany(o => o.Complaints)
+                   .HasForeignKey(c => c.OrderId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            // Complaint -> SubmittedBy (ApplicationUser)
+            builder.HasOne(c => c.SubmittedBy)
+                   .WithMany(u => u.Complaints)
+                   .HasForeignKey(c => c.SubmittedById)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            // Complaint -> ResolvedBy (ApplicationUser)
+            builder.HasOne(c => c.ResolvedBy)
+                   .WithMany(u => u.ResolvedComplaints)
+                   .HasForeignKey(c => c.ResolvedById)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Infrastructure/PharmaBridge.Persistence/Configurations/OrdersOperationsConfig/PaymentConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaBridge.Domain.Models.UserAccess;
+using PharmaBridge.Shared.EnumHelper.UserAccessEnums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Persistence.Configurations.OrdersOperationsConfig
+{
+    public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+    {
+        public void Configure(EntityTypeBuilder<Payment> builder)
+        {
+            //تم تعبئة الكرش بنجاح 
+            builder.ToTable("Payments");
+
+            builder.Property(p => p.Amount).HasPrecision(18, 2);
+            builder.Property(p => p.PaymentIntentId).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.PaymentMethod).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.GatewayName).HasMaxLength(100);
+            builder.Property(p => p.GatewayResponse).HasMaxLength(500);
+
+            builder.Property(p => p.Status)
+                   .HasConversion(
+                       v => v.ToString(),
+                       v => (PaymentStatus)Enum.Parse(typeof(PaymentStatus), v)
+                   );
+
+            // Relationships
+
+            // Payment -> Order
+            builder.HasOne(p => p.Order)
+                   .WithMany(o => o.Payments)
+                   .HasForeignKey(p => p.OrderId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
