c1c6f12f689a56ebebd12896cc60b5bbc36cd50c
Hossam Ahmed <hossam.a7med02@gmail.com>
2026-03-21 16:46:02 +0200

    Make AppliciationUser, PatientProfile and PharmaOwner configurations (#12)
    * Make AppliciationUser, PatientProfile and PharmaOwner configurations
    
    * Fix some errors
    
    ---------
    
    Co-authored-by: Hossam Ahmed <Hossam Ahmed@DESKTOP-VHO8CMP>

---
+++ Infrastructure/PharmaBridge.Persistence/Configurations/User_Profiles/ApplicationUserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Persistence.Configurations.User_Profiles
+{
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.ToTable("ApplicationUsers");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.FullName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(x => x.Role)
+                .HasMaxLength(50)
+                .IsRequired()
+                .HasConversion<string>();
+
+
+
+            //The relation of Notification, Complaints, ResolvedComplaints will be added in their classes of Config
+
+        }
+
+    }
+}
