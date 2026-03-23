020e6332cc3c0f9784bf4ec4fc06b95a19ff1282
mohamedes324 <mohamedesaam363@gmail.com>
2026-03-23 06:45:24 +0200

    Add configurations for PrescriptionRequest and Notification modules

---
+++ Infrastructure/PharmaBridge.Persistence/Configurations/PrescriptionNotifyConfig/NotificationConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaBridge.Domain.Models.UserAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Persistence.Configurations.PharmaRequestsConfig
+{
+    public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            // Table & Key
+
+            builder.ToTable("Notifications");
+
+            builder.HasKey(x => x.Id);
+
+            // Properties
+
+            builder.Property(x => x.NotifyType)
+                   .HasConversion<string>()
+                   .HasMaxLength(50)
+                   .IsRequired();
+
+            builder.Property(x => x.Title)
+                   .HasMaxLength(100)
+                   .IsRequired();
+
+            builder.Property(x => x.Description)
+                   .HasMaxLength(500)
+                   .IsRequired();
+
+            builder.Property(x => x.ReferenceId);
+
+            builder.Property(x => x.ReferenceType)
+                   .HasMaxLength(100);
+
+            builder.Property(x => x.IsRead)
+                   .IsRequired();
+
+            // Relationships
+
+            // Notification (Many) → (1) ApplicationUser
+
+            builder.HasOne(x => x.ApplicationUser)
+                   .WithMany(u => u.Notifications)
+                   .HasForeignKey(x => x.ApplicationUserId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Infrastructure/PharmaBridge.Persistence/Configurations/PrescriptionNotifyConfig/PrescriptionRequestConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaBridge.Domain.Models.Pharma_Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Persistence.Configurations.PharmaRequestsConfig
+{
+    public class PrescriptionRequestConfiguration : IEntityTypeConfiguration<PrescriptionRequest>
+    {
+        public void Configure(EntityTypeBuilder<PrescriptionRequest> builder)
+        {
+            // Table & Key
+
+            builder.ToTable("PrescriptionRequests");
+
+            builder.HasKey(x => x.Id);
+
+            // Properties
+
+            builder.Property(x => x.ImageUrl)
+                   .HasMaxLength(500);
+
+            builder.Property(x => x.PatientNotes)
+                   .HasMaxLength(500);
+
+            builder.Property(x => x.MedicineName)
+                   .HasMaxLength(100);
+
+            builder.Property(x => x.Status)
+                   .HasConversion<string>()
+                   .HasMaxLength(50)
+                   .IsRequired();
+
+            builder.Property(x => x.ExpiresAt)
+                   .IsRequired();
+
+            // Relationships
+
+            // PrescriptionRequest (Many) → (1) PatientProfile
+            builder.HasOne(x => x.PatientProfile)
+                   .WithMany(p => p.PrescriptionRequests)
+                   .HasForeignKey(x => x.PatientProfileId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            // PrescriptionRequest (Many) → (1) DeliveryAddress
+            builder.HasOne(x => x.DeliveryAddress)
+                   .WithMany()
+                   .HasForeignKey(x => x.DeliveryAddressId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
