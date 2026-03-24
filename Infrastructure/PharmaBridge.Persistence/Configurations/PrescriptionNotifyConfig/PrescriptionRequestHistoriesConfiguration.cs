352b2b92c6a1a951e119549835ef067ba9d3e395
Mohamed Esaam <112281690+mohamedes324@users.noreply.github.com>
2026-03-24 18:51:19 +0200

    Add configurations for PrescriptionRequest and Notification modules (#14)
    * Add configurations for PrescriptionRequest and Notification modules
    
    * Change delete behavior to cascade for PrescriptionRequest
    
    if request delete the history must deleted like Bid and BidItmes
    
    * Remove ReferenceId property from NotificationConfiguration
    
    Removed the ReferenceId property from the NotificationConfiguration.
    
    ---------
    
    Co-authored-by: Sobih Mohamed <158785931+SobihMohamed@users.noreply.github.com>

---
+++ Infrastructure/PharmaBridge.Persistence/Configurations/PrescriptionNotifyConfig/PrescriptionRequestHistoriesConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PharmaBridge.Domain.Models.Pharma_Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Persistence.Configurations.PharmaRequestsConfig
+{
+    public class PrescriptionRequestHistoriesConfiguration : IEntityTypeConfiguration<PrescriptionRequestHistory>
+    {
+        public void Configure(EntityTypeBuilder<PrescriptionRequestHistory> builder)
+        {
+            // Table & Key
+
+            builder.ToTable("PrescriptionRequestHistories");
+
+            builder.HasKey(x => x.Id);
+
+            // Properties
+
+            builder.Property(x => x.Notes)
+                   .HasMaxLength(500)
+                   .IsRequired();
+
+
+            builder.Property(x => x.NewStatus)
+                   .HasMaxLength(50)
+                   .IsRequired();
+
+            builder.Property(x => x.OldStatus)
+                   .HasMaxLength(50)
+                   .IsRequired();
+
+            builder.Property(x => x.ChangedAt)
+                   .IsRequired();
+
+            // Relationships
+
+            // PrescriptionRequestHistory (Many) → (1) PrescriptionRequest
+
+            builder.HasOne(x => x.PrescriptionRequest)
+                   .WithMany(p => p.PrescriptionRequestHistorys)
+                   .HasForeignKey(x => x.PrescriptionRequestId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            // PrescriptionRequestHistory (Many) → (1) ApplicationUser (ChangedBy)
+
+            builder.HasOne(x => x.ChangedBy)
+                   .WithMany()
+                   .HasForeignKey(x => x.ChangedById)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
