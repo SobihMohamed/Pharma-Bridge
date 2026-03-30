8e2b7f48c6f3bf8b0b287619263b90fbbb075cdd
Hossam Ahmed <Hossam Ahmed@DESKTOP-VHO8CMP>
2026-03-31 01:21:14 +0200

    git commit -m "Add UserDto"

---
+++ PharmaBridge.Shared/Dtos/UserProfiles/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Shared.Dtos.UserProfiles
+{
+    public class UserDto
+    {
+        public string Token { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool IsAuthenticated { get; set; }
+    }
+}
