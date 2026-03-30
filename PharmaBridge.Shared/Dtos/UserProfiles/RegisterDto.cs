93c457de7fee3be13b6b8c69c754773da8554eb4
Hossam Ahmed <Hossam Ahmed@DESKTOP-VHO8CMP>
2026-03-31 01:18:41 +0200

    git commit -m "RegisterDto"

---
+++ PharmaBridge.Shared/Dtos/UserProfiles/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Shared.Dtos.UserProfiles
+{
+    public class RegisterDto
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+    }
+}
