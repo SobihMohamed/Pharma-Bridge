662e4679bf324e31480f2b1e1b99826d31dd3f3d
Hossam Ahmed <Hossam Ahmed@DESKTOP-VHO8CMP>
2026-03-31 19:59:30 +0200

    Login/Register/User Dtos

--- PharmaBridge.Shared/Dtos/UserProfiles/UserDto.cs
+++ PharmaBridge.Shared/Dtos/UserProfiles/UserDto.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace PharmaBridge.Shared.Dtos.UserProfiles
+namespace PharmaBridge.Shared.DTOs.UserProfiles
 {
     public class UserDto
     {
