e2f31dd8b6b69f7a0b8628641cd84c12695c7c67
Hossam Ahmed <Hossam Ahmed@DESKTOP-VHO8CMP>
2026-03-31 22:39:11 +0200

    git commit -m "Add PharmaOwnerDetailsDto"

---
+++ PharmaBridge.Shared/DTOs/PharmaOwners/PharmaOwnerDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Shared.DTOs.PharmaOwners
+{
+    public class PharmaOwnerDetailsDto
+    {
+        public string Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        public string FullName { get; set; }
+        public string? Email { get; set; } 
+        public string? PhoneNumber { get; set; }
+        public string Status { get; set; }
+    }
+}
