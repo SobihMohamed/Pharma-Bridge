4f6fdbe93624450cfabcf9def2b2873dd6b74444
Hossam Ahmed <Hossam Ahmed@DESKTOP-VHO8CMP>
2026-03-31 20:35:30 +0200

    Add Notification Dto

---
+++ PharmaBridge.Shared/DTOs/Notifications/NotificationDto.cs
@@ -0,0 +1,15 @@
+﻿using PharmaBridge.Shared.EnumHelper.UserAccessEnums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Shared.DTOs.Notifications
+{
+    public class NotificationDto
+    {
+        public NotificationType NotifyType { get; }
+        public string Title { get; }
+        public string Description { get; }
+        public bool IsRead { get; }
+    }
+}
