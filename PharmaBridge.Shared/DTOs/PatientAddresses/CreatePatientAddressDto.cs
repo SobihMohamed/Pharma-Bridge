2922ccb3840c349107df44ca75f0c82761b454ef
Hossam Ahmed <Hossam Ahmed@DESKTOP-VHO8CMP>
2026-03-31 20:32:33 +0200

    Add Patient Address Dtos

---
+++ PharmaBridge.Shared/DTOs/PatientAddresses/CreatePatientAddressDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Shared.DTOs.PatientAddresses
+{
+    public class CreatePatientAddressDto
+    {
+        public string AddressLine { get; set; }
+        public string City { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
