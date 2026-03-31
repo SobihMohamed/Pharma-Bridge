f8e4ee78fd0e206a878439a8ea4ed8b904afb5f2
Hossam Ahmed <Hossam Ahmed@DESKTOP-VHO8CMP>
2026-03-31 22:36:28 +0200

    git commit -m "Patient addresse Dtos"

--- PharmaBridge.Shared/DTOs/PatientAddresses/CreatePatientAddressDto.cs
+++ PharmaBridge.Shared/DTOs/PatientAddresses/CreatePatientAddressDto.cs
@@ -6,9 +6,20 @@ namespace PharmaBridge.Shared.DTOs.PatientAddresses
 {
     public class CreatePatientAddressDto
     {
+        [Required]
+        [MaxLength(250)]
         public string AddressLine { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string City { get; set; }
+        
         public decimal Latitude { get; set; }
+        
         public decimal Longitude { get; set; }
+        public bool IsDefault { get; set; }
+
+        [Required]
+        public string PatientProfileId { get; set; }
     }
 }
--- PharmaBridge.Shared/DTOs/PatientAddresses/UpdatePatientAddressDto.cs
+++ PharmaBridge.Shared/DTOs/PatientAddresses/UpdatePatientAddressDto.cs
@@ -6,9 +6,18 @@ namespace PharmaBridge.Shared.DTOs.PatientAddresses
 {
     public class UpdatePatientAddressDto
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string AddressLine { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string City { get; set; }
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
+        public bool IsDefault { get; set; }
     }
 }
