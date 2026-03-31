f43e21bc8eeb0066d0dcae489ce4c478229ef9be
Hossam Ahmed <Hossam Ahmed@DESKTOP-VHO8CMP>
2026-03-31 22:34:04 +0200

    Make Application Users Dtos

---
+++ PharmaBridge.Shared/DTOs/ApplicationUsers/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Shared.DTOs.ApplicationUsers
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ PharmaBridge.Shared/DTOs/ApplicationUsers/RegisterDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Shared.DTOs.ApplicationUsers
+{
+    public class RegisterDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+
+
+    }
+}
