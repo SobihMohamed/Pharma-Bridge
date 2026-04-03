346c331144b2d97e4ce065d61e7625cd2c300aa4
Sobih Mohamed <158785931+SobihMohamed@users.noreply.github.com>
2026-04-03 09:39:08 +0200

    Make PaymentIntentId property nullable
    Made PaymentIntentId nullable to allow for cases without an intent.

--- PharmaBridge.Shared/DTOs/Payment/PaymentDto.cs
+++ PharmaBridge.Shared/DTOs/Payment/PaymentDto.cs
@@ -7,13 +7,20 @@ namespace PharmaBridge.Shared.DTOs.Payment
     public class PaymentDto
     {
         public int Id { get; set; }
-        public string PaymentIntentId { get; set; }
+        public string? PaymentIntentId { get; set; }
+        
         public decimal Amount { get; set; }
+        
+        //(Succeeded, CreditCard, etc.)
         public string PaymentMethod { get; set; }
         public string Status { get; set; }
         public string? GatewayName { get; set; }
+        
         public DateTime InitiatedAt { get; set; }
         public DateTime? SettledAt { get; set; }
+
+        //Flattening:
         public int OrderId { get; set; }
+        public string PharmacyName { get; set; }
     }
 }
