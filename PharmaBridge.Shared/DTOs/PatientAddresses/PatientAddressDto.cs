80dfb016960208a5728d052ec515373892afa339
Sobih Mohamed <158785931+SobihMohamed@users.noreply.github.com>
2026-04-03 14:34:14 +0200

    Add Id and IsDefault properties to PatientAddressDto

--- PharmaBridge.Shared/DTOs/PatientAddresses/PatientAddressDto.cs
+++ PharmaBridge.Shared/DTOs/PatientAddresses/PatientAddressDto.cs
@@ -6,9 +6,11 @@ namespace PharmaBridge.Shared.DTOs.PatientAddresses
 {
     public class PatientAddressDto
     {
+        public int Id { get; set; } // CRITICAL for selection/update/delete
         public string AddressLine { get; set; }
         public string City { get; set; }
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
+        public bool IsDefault { get; set; } // CRITICAL for UI UX
     }
 }
