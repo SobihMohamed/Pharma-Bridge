b7ca1f7a7f47ff688e23866cc0a0f28ab765c5c6
Sobih Mohamed <158785931+SobihMohamed@users.noreply.github.com>
2026-04-03 08:34:01 +0200

    Modify AdminPharmacyDetailsDto structure
    Updated inheritance from PharmacyDetailsDto to PharmacyDto and added Latitude, Longitude, and ContactPhone properties.

--- PharmaBridge.Shared/DTOs/Pharmacy/AdminPharmacyDetailsDto .cs
+++ PharmaBridge.Shared/DTOs/Pharmacy/AdminPharmacyDetailsDto .cs
@@ -4,12 +4,19 @@ using System.Text;
 
 namespace PharmaBridge.Shared.DTOs.Pharmacy
 {
-    public class AdminPharmacyDetailsDto : PharmacyDetailsDto
+    public class AdminPharmacyDetailsDto : PharmacyDto
     {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        // public string? Address { get; set; } it already exist but in this get the all string
+    
         public string LicenseNumber { get; set; }
         public string Status { get; set; }
-        public int CompleteOrderCount { get; set; }
         public string? RejectedReasons { get; set; }
-        public string PharmaOwnerId { get; set; }
+        public string? ContactPhone { get; set; }
+    
+        // Flattening owner data
+        public string OwnerName { get; set; }
+        public string OwnerEmail { get; set; }
     }
 }
