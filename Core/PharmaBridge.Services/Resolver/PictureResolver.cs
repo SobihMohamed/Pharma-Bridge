e7f608f62cf695cd6fd12174d2740e5d9fa773e2
Sobih <sobihmohamedsobih@gmail.com>
2026-04-10 23:11:36 +0200

    Add PictureResolver for image URLs and ApiBaseUrl setting
    Implemented PictureResolver<TSource, TDestination> to generate full image URLs using a configurable ApiBaseUrl. Added the ApiBaseUrl setting to appsettings.json for dynamic URL construction.

---
+++ Core/PharmaBridge.Services/Resolver/PictureResolver.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Services.Resolver
+{
+    public class PictureResolver<TSource, TDestination> (IConfiguration configuration) : IMemberValueResolver<TSource, TDestination, string, string>
+    {
+        public string Resolve(TSource source, TDestination destination, string sourceMember, string destMember, ResolutionContext context)
+        {
+            if(string.IsNullOrEmpty(sourceMember))
+                return string.Empty;
+
+            var baseUrl = configuration["ApiBaseUrl"];
+            return $"{baseUrl}/{sourceMember.TrimStart('/')}"; // http://localhost:5000/images/product1.jpg
+        }
+    }
+}
