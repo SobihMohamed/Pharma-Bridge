bac6778602a3078572451d02371cf8613ab581d7
Nour Sameh <nono5noor4@gmail.com>
2026-04-24 03:20:49 +0300

    done

--- PharmaBridge.Web/Extensions/ApplicationServiceExtension.cs
+++ PharmaBridge.Web/Extensions/ApplicationServiceExtension.cs
@@ -1,10 +1,9 @@
 using PharmaBridge.Abstraction.IServices.Attachement;
 using PharmaBridge.Abstraction.IServices.Auth;
+using PharmaBridge.Abstraction.IServices.Order;
 using PharmaBridge.Abstraction.IServices.Pharmacy;
 using PharmaBridge.Abstraction.IServices.PrescriptionRequest;
 using PharmaBridge.Abstraction.IServices.Token;
-using PharmaBridge.Abstraction.IServices.Order;
-using PharmaBridge.Abstraction.IServices.Pharmacy;
 using PharmaBridge.Domain.Contracts.GenericReposPattern;
 using PharmaBridge.Domain.Contracts.UnitOfWorkPattern;
 using PharmaBridge.Domain.DbInitializer;
@@ -14,12 +13,11 @@ using PharmaBridge.Persistence.Implementations.UoWPattern;
 using PharmaBridge.Services.Resolver;
 using PharmaBridge.Services.ServicesImplementation.Attachement;
 using PharmaBridge.Services.ServicesImplementation.Auth;
+using PharmaBridge.Services.ServicesImplementation.OrderService;
 using PharmaBridge.Services.ServicesImplementation.Pharmacy;
 using PharmaBridge.Services.ServicesImplementation.PrescriptionRequest;
 using SoftBridge.Services.Services.Token;
 using System.Text.Json.Serialization;
-using PharmaBridge.Services.ServicesImplementation.OrderService;
-using PharmaBridge.Services.ServicesImplementation.Pharmacy;
 
 namespace PharmaBridge.Web.Extensions
 {
@@ -29,13 +27,22 @@ namespace PharmaBridge.Web.Extensions
         {
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IAuthService, AuthService>();
-            services.AddScoped<IAttachementService ,AttachmentService>();
+            services.AddScoped<IAttachementService, AttachmentService>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IDbInitializer, DbInitialized>();
             services.AddScoped<IPrescriptionRequestService, PrescriptionRequestService>();
             services.AddScoped<IPharmacyProfileService, PharmacyProfileService>();
-            
-            
+            services.AddScoped<IOrderService, OrderService>();
+
+
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    // convert the enum from num to string
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
+
+            services.AddTransient(typeof(PictureResolver<,>));
             return services;
         }
     }
