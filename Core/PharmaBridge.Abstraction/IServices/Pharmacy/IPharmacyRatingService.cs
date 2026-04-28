e76a684844cc2bd838fda0282fb48d465878e774
Mohammed Adel <145216355+MohammedAdel110@users.noreply.github.com>
2026-04-28 14:52:23 +0300

    update ipharmarating interface

--- Core/PharmaBridge.Abstraction/IServices/Pharmacy/IPharmacyRatingService.cs
+++ Core/PharmaBridge.Abstraction/IServices/Pharmacy/IPharmacyRatingService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PharmaBridge.Shared.DTOs.PharmacyRating;
+using PharmaBridge.Shared.Common.Pagination;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PharmaBridge.Abstraction.IServices.Pharmacy
 {
@@ -12,7 +15,7 @@ namespace PharmaBridge.Abstraction.IServices.Pharmacy
 
         // Patient submits a rating (1-5 stars) and an optional comment.
         // Validation needed inside: Check if Order is Completed and belongs to this Patient or not.
-        //Task<bool> SubmitRatingAsync(CreatePharmacyRatingDto createRatingDto, Guid patientId);
+        Task<bool> SubmitRatingAsync(CreatePharmacyRatingDto createRatingDto, string patientId);
 
 
         // =========================================================================
@@ -20,6 +23,6 @@ namespace PharmaBridge.Abstraction.IServices.Pharmacy
         // =========================================================================
 
         // Get paginated list of reviews for a specific pharmacy (To display on their profile).
-        //Task<Pagination<PharmacyRatingDto>> GetPharmacyReviewsAsync(Guid pharmacyId);
+        Task<PaginationResponse<PharmacyRatingDto>> GetPharmacyReviewsAsync(int pharmacyId);
     }
 }
