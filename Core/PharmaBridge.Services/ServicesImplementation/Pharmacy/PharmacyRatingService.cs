68b7014a3f6f7aea1c20f8d9ebbea28dd943bc92
Mohammed Adel <145216355+MohammedAdel110@users.noreply.github.com>
2026-04-28 15:02:02 +0300

    create Rating Specification

---
+++ Core/PharmaBridge.Services/ServicesImplementation/Pharmacy/PharmacyRatingService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Services.ServicesImplementation.Pharmacy
+{
+    internal class PharmacyRatingService
+    {
+    }
+}
---
+++ Core/PharmaBridge.Services/Specifications/Pharmacy/PharmacyRatingsByPharmacyIdSpec.cs
@@ -0,0 +1,14 @@
+﻿using PharmaBridge.Domain.Contracts.SpecificationPattern.BaseSpec;
+using PharmaBridge.Domain.Contracts.UnitOfWorkPattern;
+using PharmaBridge.Domain.Models.UserAccess;
+
+namespace PharmaBridge.Services.Specifications.Ratings
+{
+    public class PharmacyRatingsByPharmacyIdSpec : BaseSpecifications<PharmacyRating, int>
+    {
+        public PharmacyRatingsByPharmacyIdSpec(int pharmacyId)
+            : base(r => r.PharmacyId == pharmacyId)
+        {
+        }
+    }
+}
