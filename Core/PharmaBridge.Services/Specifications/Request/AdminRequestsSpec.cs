472fe66afd7b165eec102b35be561b13ab7cba80
Sobih Mohamed <158785931+SobihMohamed@users.noreply.github.com>
2026-06-10 16:18:47 +0300

    Add admin and pharmacy operations with refactoring (#32)
    - Added new DTOs for admin and pharmacy-specific operations.
    - Updated `IPrescriptionRequestService` with new methods.
    - Refactored `PrescriptionRequestService` for better modularity.
    - Introduced new specifications for filtering and pagination.
    - Enhanced `PrescriptionRequestProfile` with new DTO mappings.
    - Added helper methods for validation and processing logic.
    - Updated `PrescriptionRequestQueryParams` to support `PatientId`.
    - Removed redundant code and improved reusability.
    - Added binary files for testing or demonstration purposes.
    
    Co-authored-by: Sobih <sobihmohamedsobih@gmail.com>

---
+++ Core/PharmaBridge.Services/Specifications/Request/AdminRequestsSpec.cs
@@ -0,0 +1,26 @@
+﻿using PharmaBridge.Domain.Models.Pharma_Requests;
+using PharmaBridge.Domain.Models.User;
+using PharmaBridge.Shared.Common.Params.PrescriptionRequest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Services.Specifications.Request
+{
+    public sealed class AdminRequestsSpec : AdminRequestsCountSpec
+    {
+        public AdminRequestsSpec(PrescriptionRequestQueryParams p) : base(p)
+        {
+            AddInclude(r => r.DeliveryAddress);
+            AddInclude(r => r.PatientProfile);
+
+            var UserNavigate = $"{nameof(PrescriptionRequestEntity.PatientProfile)}.{nameof(PatientProfile.ApplicationUser)}";
+            AddInclude(UserNavigate);
+
+            AddInclude(r => r.Bids);
+
+            AddOrderBy(r => r.CreatedAt,true);
+            ApplyPaging(p.PageSize, p.PageIndex);
+        }
+    }
+}
---
+++ PharmaBridge.Shared/DTOs/PharmaRequests/AdminReq/AdminBidDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaBridge.Shared.DTOs.PharmaRequests.AdminReq
+{
+    public class AdminBidDto
+    {
+        public int Id { get; set; }
+        public string PharmacyName { get; set; } = string.Empty;
+        public string PharmacyPhone { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public decimal TotalPrice { get; set; }
+        public DateTime SubmittedAt { get; set; }
+        public List<AdminBidItemDto> BidItems { get; set; } = new();
+    }
+}
