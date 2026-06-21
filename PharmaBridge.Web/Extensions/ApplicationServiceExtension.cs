f74056c2a5a480b093bc8aea94d8f91af86b3e8d
Sobih <sobihmohamedsobih@gmail.com>
2026-06-21 19:38:14 +0300

    refactor code from bugs

--- PharmaBridge.Web/Extensions/ApplicationServiceExtension.cs
+++ PharmaBridge.Web/Extensions/ApplicationServiceExtension.cs
@@ -3,7 +3,6 @@ using PharmaBridge.Abstraction.IServices.Attachement;
 using PharmaBridge.Abstraction.IServices.Auth;
 using PharmaBridge.Abstraction.IServices.Bidding;
 using PharmaBridge.Abstraction.IServices.Complaint;
-using PharmaBridge.Abstraction.IServices.CurrentUser;
 using PharmaBridge.Abstraction.IServices.Notification;
 using PharmaBridge.Abstraction.IServices.Order;
 using PharmaBridge.Abstraction.IServices.Pharmacy;
@@ -12,7 +11,6 @@ using PharmaBridge.Abstraction.IServices.Token;
 using PharmaBridge.Domain.Contracts.UnitOfWorkPattern;
 using PharmaBridge.Domain.DbInitializer;
 using PharmaBridge.Persistence.Implementations.InitializerImplement;
-using PharmaBridge.Persistence.Implementations.ReposPattern;
 using PharmaBridge.Persistence.Implementations.SpecificReposPattern;
 using PharmaBridge.Domain.Contracts.SpecificReposPattern;
 using PharmaBridge.Persistence.Implementations.UoWPattern;
@@ -21,7 +19,6 @@ using PharmaBridge.Services.Resolver;
 using PharmaBridge.Services.ServicesImplementation.Attachement;
 using PharmaBridge.Services.ServicesImplementation.Auth;
 using PharmaBridge.Services.ServicesImplementation.Complaint;
-using PharmaBridge.Services.ServicesImplementation.CurrentUser;
 using PharmaBridge.Services.ServicesImplementation.Notification;
 using PharmaBridge.Services.ServicesImplementation.Notification.StrategyPattern;
 using PharmaBridge.Services.ServicesImplementation.OrderService;
@@ -29,6 +26,7 @@ using PharmaBridge.Services.ServicesImplementation.Pharmacy;
 using PharmaBridge.Services.ServicesImplementation.PrescriptionRequest;
 using PharmaBridge.Web.Hubs;
 using SoftBridge.Services.Services.Token;
+using System.Text.Json.Serialization;
 
 namespace PharmaBridge.Web.Extensions
 {
@@ -40,9 +38,9 @@ namespace PharmaBridge.Web.Extensions
             // 1. Core & Infrastructure
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IDbInitializer, DbInitialized>();
+
             // 2. (Strategy Pattern)
             services.AddScoped<INotificationStrategy, PushedNotificationStrategy>();
-            //services.AddScoped<INotificationStrategy, EmailNotificationStrategy>();
 
             // Notification Hubs
             services.AddScoped<IWebNotificationPusher, WebNotificationPusher>();
@@ -52,14 +50,12 @@ namespace PharmaBridge.Web.Extensions
             services.AddScoped<INotificationService, NotificationService>();
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<ITokenService, TokenService>();
-            services.AddScoped<ICurrentUserService, CurrentUserService>();
             services.AddScoped<IAttachementService, AttachmentService>();
             services.AddScoped<IPrescriptionRequestService, PrescriptionRequestService>();
             services.AddScoped<IPharmacyProfileService, PharmacyProfileService>();
             services.AddScoped<IBidService, BidService>();
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<IPharmacyRatingService, PharmacyRatingService>();
-            services.AddScoped<IPharmacyDashboardService, PharmacyDashboardService>();
             services.AddScoped<IOrderRepository, OrderRepository>();
             services.AddScoped<IComplaintService, ComplaintService>();
 
