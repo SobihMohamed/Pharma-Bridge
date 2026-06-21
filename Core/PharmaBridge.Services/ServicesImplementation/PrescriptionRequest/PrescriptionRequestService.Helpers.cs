6615136447947140277384d96be000886dcb1aeb
Sobih <sobihmohamedsobih@gmail.com>
2026-06-21 19:38:14 +0300

    refactor code from bugs

--- Core/PharmaBridge.Services/ServicesImplementation/PrescriptionRequest/PrescriptionRequestService.Helpers.cs
+++ Core/PharmaBridge.Services/ServicesImplementation/PrescriptionRequest/PrescriptionRequestService.Helpers.cs
@@ -50,10 +50,10 @@ namespace PharmaBridge.Services.ServicesImplementation.PrescriptionRequest
             return request;
         }
 
-        private async Task<PatientAddress> GetValidAddressAsync(int addressId, string patientId)
+        private async Task<Domain.Models.User.PatientAddress> GetValidAddressAsync(int addressId, string patientId)
         {
             var addressSpec = new PatientAddressWithPatientprofileSpec(addressId, patientId);
-            var addressRepo = unitOfWork.GetRepository<PatientAddress, int>();
+            var addressRepo = unitOfWork.GetRepository<Domain.Models.User.PatientAddress, int>();
 
             var deliveryAddress = await addressRepo.GetByIdWithSpecAsync(addressSpec);
             if (deliveryAddress == null) throw new BadRequestCustomeException("Invalid delivery address");
